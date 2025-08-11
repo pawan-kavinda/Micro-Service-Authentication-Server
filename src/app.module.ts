725ac8007878b526b0b3cc99f097b0675016f79d
pawan-kavinda <pawankavinda09@gmail.com>
2025-08-11 16:51:27 +0530

    v

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI'),
-      }),
-      inject: [ConfigService],
-    }),
+    MongooseModule.forRoot(
+      'mongodb+srv://buwa26889:26889@cluster0.ufhroq5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
     AuthModule,
     UserModule,
   ],
